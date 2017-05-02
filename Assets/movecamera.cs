1273c3185896e38c00941056ba8e6ffe72b0e430
Jastine Goyena <goyenaja@mcmaster.ca>
2017-05-02 14:48:58 -0400

    Added background music
    Claire de Lune is relaxing

---
+++ Assets/movecamera.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movecamera : MonoBehaviour {
+
+    public GameObject objectMars;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
