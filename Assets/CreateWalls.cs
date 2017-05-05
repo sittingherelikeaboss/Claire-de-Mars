e7425ce8feb7aabc4b92c5e6cebd6c12254592e5
Jastine Goyena <goyenaja@mcmaster.ca>
2017-05-04 23:32:01 -0400

    added planes

---
+++ Assets/CreateWalls.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateWalls : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        getInput();
+	}
+
+    void getInput()
+    {
+        if(GvrViewer.Instance.Triggered == true)
+        {
+
+        }
+    }
+}
